d109d61e18df6b35048b442e00dd6a370efbfd76
Edivaldo Davi <edidavi@gmail.com>
2023-07-21 00:05:12 -0300

    corrigido erro prisma com add postinstall no packagejson

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,86 @@
-import express, { Express, Request, Response, } from "express"
-import { Router } from "express"
+import { PrismaClient } from "@prisma/client"
+import dotenv from "dotenv"
+import express, { Express, Request, Response } from "express"
 
+import { CustomerData } from "./interfaces/CustomerData"
+import { PaymentData } from "./interfaces/PaymentData"
+import { SnackData } from "./interfaces/SnackData"
+
+import CheckoutService from "./services/CheckoutService"
+
+import cors from 'cors'
+
+dotenv.config()
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}
 const app: Express = express()
 const port = process.env.PORT || 5000
-const route = Router();
+const prisma = new PrismaClient()
+
+app.use(express.json())
+app.use(cors())
 app.get("/", (req: Request, res: Response) => {
   const { message } = req.body
 
   if (!message) return res.status(400).send({ error: "Message is required" })
 
-  res.send({ message }) })
-  app.use(route)
+  res.send({ message })
+})
+
+app.get("/snacks", cors(corsOptions), async (req: Request, res: Response) => {
+  const { snack } = req.query
+
+  if (!snack) return res.status(400).send({ error: "Snack is required" })
+
+  // SELECT * FROM Snack WHERE snack = 'drink'
+  const snacks = await prisma.snack.findMany({
+    where: {
+      snack: {
+        equals: snack as string,
+      },
+    },
+  })
+
+  res.send(snacks)
+})
+
+app.get("/orders/:id", cors(corsOptions), async (req: Request, res: Response) => {
+  const { id } = req.params
+
+  const order = await prisma.order.findUnique({
+    where: {
+      id: +id,
+    },
+    include: { customer: true, orderItems: { include: { snack: true } } },
+  })
+
+  if (!order) return res.status(404).send({ error: "Order not found" })
+
+  res.send(order)
+})
+
+interface CheckoutRequest extends Request {
+  body: {
+    cart: SnackData[]
+    customer: CustomerData
+    payment: PaymentData
+  }
+}
+
+app.post("/checkout", cors(corsOptions), async (req: CheckoutRequest, res: Response) => {
+  const { cart, customer, payment } = req.body
+
+  const orderCreated = await new CheckoutService().process(
+    cart,
+    customer,
+    payment
+  )
+
+  res.send(orderCreated)
+})
 
 app.listen(port, () => {
-  console.log(`Server rodando na porta ${port}`)
+  console.log(`Server running on port ${port}`)
 })
