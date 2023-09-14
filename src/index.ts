73f6915b70baf92fa34ea7854aaab22d8bb07c71
Edivaldo Davi <edidavi@gmail.com>
2023-09-14 19:43:40 -0300

    alterado url para vercel cors

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const prisma = new PrismaClient()
 app.use(express.json())
 
 app.use((req: Request, res: Response, next ) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+  res.header("Access-Control-Allow-Origin", "https://efood-swart.vercel.app")
   app.use(cors());
   next();
 })
